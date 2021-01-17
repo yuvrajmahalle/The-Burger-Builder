048b0f298e2ffae279f7f69c04b78220d7b96f6e
yuvrajmahalle <aashishmahalle90@gmail.com>
2021-01-17 15:49:43 +0530

    added hoc and other basic building layout for the project

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+import Aux from '../../hoc/Auxi';
+
+class BurgerBuilder extends Component {
+    render(){
+        return(
+            <Aux>
+                <div>Bruger</div>
+                <div>Build Control</div>
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
