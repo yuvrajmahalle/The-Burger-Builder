99e2a13fe04f8562e7846efeb189486d8e1cce37
yuvrajmahalle <aashishmahalle90@gmail.com>
2021-01-18 21:55:15 +0530

    changed local project structure

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,28 @@
 import React,{Component} from 'react';
 import Aux from '../../hoc/Auxi';
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+/*
+    constructor(props){
+        super(props);
+        this.state ={../}
+    }
+*/
+
+    state = {
+        ingredients: {
+            salad:1,
+            bacon:1,
+            cheese:2,
+            meat:2
+        }
+    }
+
     render(){
         return(
             <Aux>
-                <div>Bruger</div>
+                <Burger ingredients={this.state.ingredients}/>
                 <div>Build Control</div>
             </Aux>
         );
