ea041b346c1ece73d15be76f05727844f8951e75
yuvrajmahalle <aashishmahalle90@gmail.com>
2021-01-20 21:45:13 +0530

    changed the folder structure

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Aux from '../../../hoc/Auxi';
+import Aux from '../../../hoc/Auxi/Auxi';
 
 import Button from '../../UI/Button/Button';
 
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import Aux from '../../hoc/Auxi';
+import Aux from '../Auxi/Auxi';
 import classes from './Layout.css';
-import Toolbar from '../Navigation/Toolbar/Toolbar';
-import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component{
 
@@ -18,9 +18,9 @@ class Layout extends Component{
         this.setState(( prevState ) => {
             return { showSideDrawer: !prevState.showSideDrawer};
         });
-        
+          
     }
-
+   
     render(){
         return(
             <Aux>
