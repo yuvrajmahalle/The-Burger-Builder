a314c0265c59ff9ee41054ede7461aae38ddb841
yuvrajmahalle <aashishmahalle90@gmail.com>
2021-01-26 08:38:48 +0530

    handled form submission and passed the data to firebase

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -67,12 +67,18 @@ class ContactData extends Component{
 
     orderHandler = (event) =>{
         event.preventDefault();
-
+        
         this.setState({loading:true})
+
+        const formData = {};
+        for(let formElementIdentifier in this.state.orderForm){
+            formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+        }
+
         const order = {
             ingredients : this.props.ingredients,
-            price: this.props.price
-            
+            price: this.props.price,
+            orderData : formData
         }
         axios.post('/orders.json',order)
         .then(response => {
@@ -108,14 +114,14 @@ class ContactData extends Component{
              })
         }
 
-        let form = (<form>
+        let form = (<form onSubmit={this.orderHandler}>
             
             {formElementsArray.map(formElement => (
                 <Input changed={(event) => this.inputChangedHandler(event,formElement.id)}
                 key={formElement.id}
                     elementType={formElement.config.elementType} elementConfig={formElement.config.elementConfig} value={formElement.config.value}/>
             ))}
-            <Button clicked={this.orderHandler} btnType="Success">ORDER</Button>
+            <Button  btnType="Success">ORDER</Button>
         </form>);
         if(this.state.loading){
             form = <Spinner />;
