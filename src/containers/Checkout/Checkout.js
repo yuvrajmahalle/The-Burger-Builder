9781ac2b146d7e7368ea7c94c7b1b5e2b56f0f44
yuvrajmahalle <aashishmahalle90@gmail.com>
2021-01-28 13:51:15 +0530

    added the functionality of combining reducers and added logic for handling async request in redux using thunk

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,38 +1,46 @@
-import React from 'react';
-import {Route} from 'react-router-dom';
+import React, { Component } from 'react';
+import { Route, Redirect } from 'react-router-dom';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
-import {connect} from 'react-redux';
-//====================================
-const Checkout = props => {
-    
-//--------------------------------------
-  const checkoutCancelledHandler = () => {
-    props.history.goBack();
+import { connect } from 'react-redux';
+
+class Checkout extends Component {
+
+  checkoutCancelledHandler = () => {
+    this.props.history.goBack();
   };
-  const checkoutContinuedHandler = () => {
-    props.history.replace('/checkout/contact-data');
+  checkoutContinuedHandler = () => {
+    this.props.history.replace('/checkout/contact-data');
   };
-  return (
-    <div>
-      <CheckoutSummary
-        ingredients={props.ings}
-        checkoutCancelled={checkoutCancelledHandler}
-        checkoutContinued={checkoutContinuedHandler}
-      />
-      <Route 
-        path={props.match.path + '/contact-data'} 
-        component={ContactData} 
-      />
-    </div>
-  );
-};
 
 
-const mapStateToProps = state =>{
+  render() {
+
+    let summary = <Redirect to="/" />;
+    if (this.props.ings) {
+      summary = (
+        <div>
+          <CheckoutSummary
+            ingredients={this.props.ings}
+            checkoutCancelled={this.checkoutCancelledHandler}
+            checkoutContinued={this.checkoutContinuedHandler}
+          />
+          <Route
+            path={this.props.match.path + '/contact-data'}
+            component={ContactData} />
+        </div>
+      );
+    }
+
+    return summary;
+  };
+}
+
+
+const mapStateToProps = state => {
   return {
-    ings:state.ingredients,
-    price:state.ingredients
+    ings: state.burgerBuilder.ingredients,
+    price: state.burgerBuilderingredients
   }
 }
 
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -7,6 +7,8 @@ import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css';
 import axios from '../../../axios-orders';
 import Input from '../../../components/UI/Input/Input';
+import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';  
+import * as actions from '../../../store/actions/index';
 
 class ContactData extends Component{
     state = {
@@ -88,13 +90,13 @@ class ContactData extends Component{
                        {value:'cheapest', displayValue:'Cheapest'},
                    ]
                 },
-                value:'',
+                value:'fastest',
                 validation :{},
                 valid:true
             },
         },
-        formIsValid:false,
-        loading: false
+        formIsValid:false
+        
     }
 
     checkValidity(value,rules){
@@ -131,14 +133,8 @@ class ContactData extends Component{
             price: this.props.price,
             orderData : formData
         }
-        axios.post('/orders.json',order)
-        .then(response => {
-            this.setState({loading:false});
-            this.props.history.push('/');
-        })
-        .catch(error => {
-            this.setState({loading:false})
-        });
+        
+        this.props.onOrderBurger(order);
 
         
     }
@@ -189,7 +185,7 @@ class ContactData extends Component{
             ))}
             <Button disabled={!this.state.formIsValid}  btnType="Success">ORDER</Button>
         </form>);
-        if(this.state.loading){
+        if(this.props.loading){
             form = <Spinner />;
         }
 
@@ -207,10 +203,17 @@ class ContactData extends Component{
 }
 const mapStateToProps =state=>{
     return{
-        ings:state.ingredients,
-        price:state.totalPrice
+        ings:state.burgerBuilder.ingredients,
+        price:state.burgerBuilder.totalPrice,
+        loading: state.order.loading
     }
 }
 
+const mapDispatchToProps = dispatch =>{
+    return{
+        onOrderBurger: (orderData)=> dispatch(actions.purchaseBurger(orderData))
+    };
+    
+}
 
-export default connect(mapStateToProps)(ContactData);
+export default connect(mapStateToProps,mapDispatchToProps)(withErrorHandler(ContactData,axios));
