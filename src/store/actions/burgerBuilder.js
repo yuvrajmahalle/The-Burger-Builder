3540fcbd6a02fdd0ee47fcf9a258ea38a389e95c
yuvrajmahalle <aashishmahalle90@gmail.com>
2021-01-28 15:41:24 +0530

    code refactored reducers and cleaned project

--- src/store/actions/burgerBuilder.js
+++ src/store/actions/burgerBuilder.js
@@ -1,35 +1,32 @@
 import * as actionTypes from './actionTypes';
 import axios from '../../axios-orders';
 
-
-export const addIngredient = (name) =>{
-    return{
-        type:actionTypes.ADD_INGREDIENT,
-        ingredientName : name
-    };
-};
-
-
-export const removeIngredient = (name) =>{
-    return{
-        type:actionTypes.REMOVE_INGREDIENT,
-        ingredientName : name
-    };
-};
-
-export const setIngredients = (ingredients) =>{
+export const addIngredient = ( name ) => {
     return {
-        type:actionTypes.SET_INGREDIENTS,
-        ingredients:ingredients
-    }
-}
+        type: actionTypes.ADD_INGREDIENT,
+        ingredientName: name
+    };
+};
+
+export const removeIngredient = ( name ) => {
+    return {
+        type: actionTypes.REMOVE_INGREDIENT,
+        ingredientName: name
+    };
+};
+
+export const setIngredients = ( ingredients ) => {
+    return {
+        type: actionTypes.SET_INGREDIENTS,
+        ingredients: ingredients
+    };
+};
 
 export const fetchIngredientsFailed = () => {
     return {
         type: actionTypes.FETCH_INGREDIENTS_FAILED
-    }
-}
-
+    };
+};
 export const initIngredients = () =>{
     return dispatch => {
         axios.get('https://react-my-burger-a928a-default-rtdb.firebaseio.com/ingredients.json')
