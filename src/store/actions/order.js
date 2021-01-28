0b84f53e00777e833b8b3a58fe8f57c7712ac0b5
yuvrajmahalle <aashishmahalle90@gmail.com>
2021-01-28 15:26:57 +0530

    fetched orders via redux

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -42,3 +42,44 @@ export const purchaseInit = () =>{
         type:actionTypes.PURCHASE_INTI
     }
 }
+
+export const fetchOrdersSucces = (orders) =>{
+    return{
+        type:actionTypes.FETCH_ORDERS_SUCCESS,
+        orders:orders
+    };
+};
+
+
+export const fetchOrdersFail = (error) =>{
+    return{
+        type:actionTypes.FETCH_ORDERS_FAIL,
+        error:error
+    };
+};
+
+export const fetchOrdersStart = () =>{
+    return{
+        type:actionTypes.FETCH_ORDERS_START
+    };
+};
+
+export const fetchOrders = () => {
+    return dispatch => {
+        dispatch(fetchOrdersStart());
+        axios.get('./orders.json')
+        .then(res=>{
+            const fetchedOrders = [];
+            for(let key in res.data){
+                fetchedOrders.push({
+                        ...res.data[key],
+                        id:key
+                })
+            }
+            dispatch(fetchOrdersSucces(fetchedOrders));
+        })
+        .catch(error=>{
+           dispatch(fetchOrdersFail(error))
+        })
+    }
+}
