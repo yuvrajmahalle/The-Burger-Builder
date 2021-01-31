d7497b8133c2dacfd2e8bcffbbb445e04052b629
yuvrajmahalle <aashishmahalle90@gmail.com>
2021-01-31 07:36:48 +0530

    added logout, restrict functionality for unAuth users

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -22,6 +22,21 @@ export const authSucces = (token, userId) => {
     }
 }
 
+export const logout = () =>{
+    return{
+        type: actionTypes.AUTH_LOGOUT
+    }
+}
+
+export const checkAuthTimeout = (expirationTime) => {
+    return dispatch => {
+        debugger;
+        setTimeout(() =>{
+            dispatch(logout());
+        },expirationTime * 1000)
+    }
+}   
+
 export const auth = (email, password, isSignup) =>  {
     return dispatch => {
         dispatch(authStart());
@@ -38,7 +53,9 @@ export const auth = (email, password, isSignup) =>  {
         axios.post(url,authData)
         .then(response => {
             console.log(response);
+            
             dispatch(authSucces(response.data.idToken, response.data.localId));
+            dispatch(checkAuthTimeout(response.data.expiresIn));
         })
         .catch(error=>{
             console.log(error);
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -11,5 +11,6 @@ export {
 } from './order'
 
 export {
-    auth
-} from './auth'
+    auth,
+    logout  
+} from './auth'   
